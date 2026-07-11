e8d6c4f9c32ba4f62e3be362ad8e853459708f02
Victor <71899727+Victor-Krupeichenko@users.noreply.github.com>
2026-07-11 14:18:59 +0300

    urls.py

--- territorial_center/urls.py
+++ territorial_center/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('', include('loevtcson.urls')),
     path('cckeditor5/', include('django_ckeditor_5.urls'))
 ]
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

3f9ad8a667178527166df589bc1a88f9b21004b7
Victor <71899727+Victor-Krupeichenko@users.noreply.github.com>
2026-07-11 13:56:13 +0300

    заменен STATICFILES_STORAGE

--- territorial_center/settings.py
+++ territorial_center/settings.py
@@ -155,7 +155,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # Если что-то будет не так со статикой, то закомментирую этот код и раскомментирую верхний STATICFILES_STORAGE
 # Хранилище статики заменено на стандартное (без WhiteNoise)
 # STORAGES = {

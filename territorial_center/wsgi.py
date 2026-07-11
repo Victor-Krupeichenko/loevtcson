ee2f14ae7bcfb856ed5a3d4f3acce329ca703ef2
Victor <71899727+Victor-Krupeichenko@users.noreply.github.com>
2026-07-11 14:08:25 +0300

    изменений много

--- territorial_center/wsgi.py
+++ territorial_center/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'territorial_center.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), '..', 'staticfiles'))

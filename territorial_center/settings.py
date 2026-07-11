035e08eefe56382c6597093a68169afd781ece2b
Victor <71899727+Victor-Krupeichenko@users.noreply.github.com>
2026-07-11 13:20:59 +0300

    удалены ( 'localhost', '127.0.0.1') из ALLOWED_HOSTS

--- territorial_center/settings.py
+++ territorial_center/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['.railway.app', 'localhost', '127.0.0.1']  # добавьте localhost
+ALLOWED_HOSTS = ['.railway.app']  # добавьте localhost
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
 
 # Раскомментируйте для HTTPS (если используете)
